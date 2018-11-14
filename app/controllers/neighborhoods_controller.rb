12963fc84448f6d44c0f64c2d2acd664213b3006
ihollander <ianholla@gmail.com>
2018-11-14 18:03:56 -0500

    some form styling

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,29 +1,32 @@
 class NeighborhoodsController < ApplicationController
   before_action :set_criteria_options, only: %i[search]
   before_action :set_minute_options, only: %i[index]
+  before_action :current_user, only: :index
 
   # starting page for filtering distance
   def index
+    @default_address = @user.work_address
   end
 
   # page for filtering incidents
   def search
-    address = params["search"]["address"]
-    minutes = params["search"]["minutes"].to_i
+    @address = params["search"]["address"]
+    @minutes = params["search"]["minutes"].to_i
     # find neighborhood for address (origin)
     # TODO: handle errors
-    origin = Neighborhood.find_by_address(address)
+    origin = Neighborhood.find_by_address(@address)
     # check journeys table for journeys matching neighborhood_a -> and neighborhood_b <-
     # TODO: handle errors
-    neighborhood_ids = Journey.within_acceptable_duration(origin.id, minutes).map(&:neighborhood_b_id)
-    neighborhood_ids += Journey.within_acceptable_duration_reverse(origin.id, minutes).map(&:neighborhood_a_id)
+    neighborhood_ids = Journey.within_acceptable_duration(origin.id, @minutes).map(&:neighborhood_b_id)
+    neighborhood_ids += Journey.within_acceptable_duration_reverse(origin.id, @minutes).map(&:neighborhood_a_id)
     @neighborhoods = Neighborhood.find(neighborhood_ids).uniq
   end
 
   # page for displaying results
   def result
-    @criteria = params["search"]["criteria"]
-    neighborhood_ids = params["search"]["neighborhoods"]
+    byebug
+    @criteria = params["search"]["criteria"].reject{ |n| n.empty? }
+    neighborhood_ids = params["search"]["neighborhoods"].reject{ |n| n.empty? }
     @neighborhoods = Neighborhood.find(neighborhood_ids)
   end
 
